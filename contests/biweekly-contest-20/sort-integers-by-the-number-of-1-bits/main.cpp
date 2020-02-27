4d5769144d820d885246694f6694ac5728ef1878
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-02-27 20:23:33 +0900

    Solve biweekly-20 1

---
+++ contests/biweekly-contest-20/sort-integers-by-the-number-of-1-bits/main.cpp
@@ -0,0 +1,83 @@
+// https://leetcode.com/contest/biweekly-contest-20/problems/sort-integers-by-the-number-of-1-bits/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  vector<int> sortByBits(vector<int>& arr) {
+    auto get_bit_ct = [](ll a) {
+      ll res = 0;
+
+      while (a > 0) {
+        if (a % 2 == 1) res++;
+        a /= 2;
+      }
+
+      return res;
+    };
+
+    sort(all(arr), [&](int a, int b) {
+      ll a_ct = get_bit_ct(a);
+      ll b_ct = get_bit_ct(b);
+
+      if (a_ct == b_ct) return a < b;
+      return a_ct < b_ct;
+    });
+
+    return arr;
+  }
+};
