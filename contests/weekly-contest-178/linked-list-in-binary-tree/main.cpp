cf7ec9dbb06331e0b17a22facef2762fbbcaaafd
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-03-01 19:36:13 +0900

    Solve weekly-178 3

---
+++ contests/weekly-contest-178/linked-list-in-binary-tree/main.cpp
@@ -0,0 +1,89 @@
+// https://leetcode.com/contest/weekly-contest-178/problems/linked-list-in-binary-tree/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+ public:
+  bool isSubPathStrictly(ListNode* head, TreeNode* root) {
+    if (!head) return true;
+    if (!root) return false;
+    return head->val == root->val &&
+           (isSubPathStrictly(head->next, root->left) ||
+            isSubPathStrictly(head->next, root->right));
+  }
+
+  bool isSubPath(ListNode* head, TreeNode* root) {
+    if (!root) return false;
+    return isSubPathStrictly(head, root) || isSubPath(head, root->left) ||
+           isSubPath(head, root->right);
+  }
+};
