6c4934b09621e3d81d621fefdd37674f681263b0
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-07-05 13:06:24 +0900

    Solve biweekly-22 1 & 3

---
+++ contests/biweekly-contest-22/find-the-distance-value-between-two-arrays/main.cpp
@@ -0,0 +1,74 @@
+// https://leetcode.com/contest/biweekly-contest-22/problems/find-the-distance-value-between-two-arrays/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+class Solution {
+ public:
+  int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+    ll res = 0;
+
+    for (auto v1 : arr1) {
+      if (all_of(all(arr2), [&](ll v2) { return abs(v2 - v1) > d; })) {
+        ++res;
+      }
+    }
+
+    return res;
+  }
+};
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
