fb28ced6cb7d5af93a8f736a7024afefe9f421d4
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-07-05 13:04:16 +0900

    Solve weekly-183 1 & 2

---
+++ contests/weekly-contest-183/minimum-subsequence-in-non-increasing-order/main.cpp
@@ -0,0 +1,77 @@
+// https://leetcode.com/contest/weekly-contest-183/problems/minimum-subsequence-in-non-increasing-order/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+class Solution {
+ public:
+  vector<int> minSubsequence(vector<int>& nums) {
+    V<int> res;
+    ll sub_sum = 0;
+    ll half_sum = accumulate(all(nums), 0ll) / 2;
+    priority_queue<ll> pq(all(nums));
+
+    while (sub_sum <= half_sum) {
+      res.push_back(pq.top());
+      sub_sum += res.back();
+      pq.pop();
+    }
+
+    return res;
+  }
+};
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
