568f5665726750b2c58b58a365843dde3ac11da4
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-07-19 20:41:43 +0900

    Solve weekly-198

---
+++ contests/weekly-contest-198/find-a-value-of-a-mysterious-function-closest-to-target/main.cpp
@@ -0,0 +1,111 @@
+// https://leetcode.com/contest/weekly-contest-198/problems/find-a-value-of-a-mysterious-function-closest-to-target/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int closestToTarget(vector<int>& arr, int target) {
+    ll N = arr.size();
+    ll bit_ct = 25;
+    VVL bit_indices(bit_ct, VL());
+
+    rep(i, N) {
+      rep(bit, bit_ct) {
+        if (arr[i] & (1 << bit)) {
+          bit_indices[bit].push_back(i);
+        }
+      }
+    }
+
+    VL dp(N);
+    ll res = abs(arr[0] - target);
+
+    repr(i, N) {
+      rep(bit, bit_ct) {
+        if (arr[i] & (1 << bit)) continue;
+
+        auto& indices = bit_indices[bit];
+        while (!indices.empty() && indices.back() > i) {
+          dp[indices.back()] -= 1 << bit;
+          indices.pop_back();
+        }
+      }
+
+      dp[i] = arr[i];
+
+      ll ok = i;  // candidate as less than target
+      ll ng = N;
+
+      while (ng - ok > 1) {
+        ll mid = (ng + ok) / 2;
+
+        if (dp[mid] >= target) {
+          ok = mid;
+        } else {
+          ng = mid;
+        }
+      }
+
+      chmin(res, abs(target - dp[ok]));
+      if (ok + 1 < N) chmin(res, abs(target - dp[ok + 1]));
+    }
+
+    return res;
+  }
+};
---
+++ contests/weekly-contest-198/number-of-nodes-in-the-sub-tree-with-the-same-label/main.cpp
@@ -0,0 +1,124 @@
+// https://leetcode.com/contest/weekly-contest-198/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename F>
+class
+#if defined(__has_cpp_attribute) && __has_cpp_attribute(nodiscard)
+    [[nodiscard]]
+#elif defined(__GNUC__) && \
+    (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+    __attribute__((warn_unused_result))
+#endif
+    FixPoint : private F {
+ public:
+  explicit constexpr FixPoint(F && f) noexcept : F(std::forward<F>(f)) {}
+
+  template <typename... Args>
+  constexpr decltype(auto) operator()(Args&&... args) const
+#if !defined(__GNUC__) || defined(__clang__) || __GNUC__ >= 9
+      noexcept(noexcept(
+          F::operator()(std::declval<FixPoint>(), std::declval<Args>()...)))
+#endif
+  {
+    return F::operator()(*this, std::forward<Args>(args)...);
+  }
+};
+
+template <typename F>
+static inline constexpr decltype(auto) makeFixPoint(F&& f) noexcept {
+  return FixPoint<F>{std::forward<F>(f)};
+}
+
+class Solution {
+ public:
+  vector<int> countSubTrees(int N, vector<vector<int>>& edges, string labels) {
+    VVL edge_list(N, VL(0));
+
+    for (auto edge : edges) {
+      ll u = edge[0];
+      ll v = edge[1];
+
+      edge_list[u].push_back(v);
+      edge_list[v].push_back(u);
+    }
+
+    VVL memo(N, VL('z' - 'a' + 1, 0));
+
+    auto dfs = makeFixPoint([&](auto f, ll u, ll p) -> void {
+      ll char_i = labels[u] - 'a';
+
+      memo[u][char_i]++;
+
+      for (ll v : edge_list[u]) {
+        if (v == p) continue;
+
+        f(v, u);
+        rep(i, 'z' - 'a' + 1) { memo[u][i] += memo[v][i]; }
+      }
+    });
+
+    dfs(0, -1);
+
+    V<int> res(N);
+    rep(i, N) { res[i] = memo[i][labels[i] - 'a']; }
+
+    return res;
+  }
+};
