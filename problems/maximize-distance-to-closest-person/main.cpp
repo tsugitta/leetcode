ed0fad12e364d18a33cbacda5ee7f667a9837ea1
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-07-28 00:22:08 +0900

    Solve maximize-distance-to-closest-person

---
+++ problems/maximize-distance-to-closest-person/main.cpp
@@ -0,0 +1,86 @@
+// https://leetcode.com/problems/maximize-distance-to-closest-person/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+using VVL = V<VL>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int maxDistToClosest(vector<int>& seats) {
+    ll N = seats.size();
+    ll left = -1;
+    ll right = 0;
+    ll res = 0;
+
+    rep(i, N) {
+      if (seats[i]) {
+        left = i;
+        continue;
+      }
+
+      while (right < N && (right < i || seats[right] == 0)) right++;
+
+      ll left_pos = left == -1 ? -INF : left;
+      ll right_pos = right == N ? INF : right;
+
+      ll dist = min(i - left_pos, right_pos - i);
+      chmax(res, dist);
+    }
+
+    return res;
+  }
+};
