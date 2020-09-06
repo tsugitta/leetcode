44cef76244395f79d52ef70cecd455b8df70c940
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-09-06 18:32:54 +0900

    Solve weekly-205

---
+++ problems/minimum-deletion-cost-to-avoid-repeating-letters/main.cpp
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int minCost(string s, vector<int>& cost) {
+    ll res = cost[0];
+    int max_cost = cost[0];
+    int N = cost.size();
+
+    repf(i, 1, N) {
+      if (s[i] != s[i - 1]) {
+        res -= max_cost;
+        max_cost = 0;
+      }
+
+      res += cost[i];
+      chmax(max_cost, cost[i]);
+    }
+
+    res -= max_cost;
+
+    return res;
+  }
+};
