56f45d39e848a856f5923e6a42e35a3cfda70886
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-11-01 22:19:06 +0900

    Solve weekly-213

---
+++ problems/count-sorted-vowel-strings/main.cpp
@@ -0,0 +1,87 @@
+// https://leetcode.com/problems/count-sorted-vowel-strings/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int countVowelStrings(int n) {
+    // 最後の文字別の個数
+    vector<int> cts(5, 1);
+
+    while (n--) {
+      vector<int> ncts(5, 0);
+
+      rep(pi, 5) rep(ni, 5) {
+        if (pi <= ni) ncts[ni] += cts[pi];
+      }
+
+      swap(cts, ncts);
+    }
+
+    ll res = 0;
+    for (auto ct : cts) res += ct;
+    return res;
+  }
+};
