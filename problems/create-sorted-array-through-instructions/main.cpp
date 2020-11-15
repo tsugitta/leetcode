bf1f9d1c70bf76f829f2a25c6e87b95f485f98c3
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-11-15 16:47:26 +0900

    Solve weekly-214

---
+++ problems/create-sorted-array-through-instructions/main.cpp
@@ -0,0 +1,218 @@
+// https://leetcode.com/problems/create-sorted-array-through-instructions/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+struct Fenwick {
+  vector<ll> tree;
+
+  Fenwick(ll n) : tree(vector<ll>(n, 0)) {}
+
+  // sum of [0, i)
+  ll sum(ll i) {
+    ll res = 0;
+
+    for (i--; i >= 0; i = (i & (i + 1)) - 1) {
+      res += tree[i];
+    }
+
+    return res;
+  }
+
+  void add(ll i, ll n) {
+    for (ll len = tree.size(); i < len; i |= i + 1) {
+      tree[i] += n;
+    }
+  }
+};
+
+template <int MOD>
+class Fp {
+ public:
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+  constexpr Fp operator+(const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator-(const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator*(const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator/(const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator+=(const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator-=(const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator*=(const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator/=(const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator!=(const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator<<(ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+
+// const ll MOD = 998244353;
+const ll MOD = 1000000007;
+using mint = Fp<MOD>;
+
+struct combination_table {
+  // i! % MOD M
+  V<ll> fact;
+  // (i!)^(-1) MOD M
+  V<ll> finv;
+  // i^(-1) MOD M
+  V<ll> inv;
+};
+
+combination_table ct;
+
+// ref: http://drken1215.hatenablog.com/entry/2018/06/08/210000
+void prepare_combination(ll max) {
+  V<ll> fact(max + 1);
+  V<ll> finv(max + 1);
+  V<ll> inv(max + 1);
+
+  fact[0] = 1;
+  fact[1] = 1;
+  finv[0] = 1;
+  finv[1] = 1;
+  inv[1] = 1;
+
+  for (ll i = 2; i <= max; i++) {
+    fact[i] = fact[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) %
+                       MOD;  // a^(-1) ≡ -(p % a)^(-1) * (p/a) MOD p
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+
+  ct = {
+      fact,
+      finv,
+      inv,
+  };
+}
+
+mint c_m(ll n, ll k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return mint(ct.fact[n]) * ct.finv[k] * ct.finv[n - k];
+}
+
+// 重複組合せ n 種類から k 個重複を許して取る通り
+mint h_m(ll n, ll k) { return c_m((n - 1) + k, k); }
+
+mint p_m(ll n, ll k) { return c_m(n, k) * ct.fact[k]; }
+
+class Solution {
+ public:
+  int createSortedArray(vector<int>& instructions) {
+    ll MAX = 1e5;
+
+    auto bit = Fenwick(MAX + 1);
+
+    mint res = 0;
+
+    for (int num : instructions) {
+      ll l = bit.sum(num);
+      ll r = bit.sum(MAX + 1) - bit.sum(num + 1);
+      res += min(l, r);
+      bit.add(num, 1);
+    }
+
+    return res.val;
+  }
+};
---
+++ problems/get-maximum-in-generated-array/main.cpp
@@ -0,0 +1,90 @@
+// https://leetcode.com/problems/get-maximum-in-generated-array/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int getMaximumGenerated(int n) {
+    if (n == 0) return 0;
+
+    VL nums(n + 1);
+    nums[0] = 0;
+    nums[1] = 1;
+
+    ll res = 1;
+
+    for (int i = 2; i <= n; ++i) {
+      if (i % 2 == 0) {
+        nums[i] = nums[i / 2];
+      } else {
+        nums[i] = nums[i / 2] + nums[i / 2 + 1];
+      }
+
+      chmax(res, nums[i]);
+    }
+
+    return res;
+  }
+};
