c02d83309cae0f8549323b66434a255e99746964
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-12-19 22:59:40 +0900

    Solve weekly-219 1 ~ 3

---
+++ problems/stone-game-vii/main.cpp
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/stone-game-vii/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int stoneGameVII(vector<int>& stones) {
+    int N = stones.size();
+
+    VVL dp(N, VL(N));
+
+    auto dfs = [&](auto self, ll l, ll r, ll sum) -> ll {
+      if (l == r) return 0;
+      if (dp[l][r]) return dp[l][r];
+
+      ll l_res = sum - stones[l] - self(self, l + 1, r, sum - stones[l]);
+      ll r_res = sum - stones[r] - self(self, l, r - 1, sum - stones[r]);
+      return dp[l][r] = max(l_res, r_res);
+    };
+
+    ll sum = accumulate(all(stones), 0ll);
+    ll res = dfs(dfs, 0, N - 1, sum);
+
+    return res;
+  }
+};
