efe12445f9e1ac70690921be8d130f2d03906ef1
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-12-27 22:41:57 +0900

    Solve weekly-221 1 ~ 3

---
+++ problems/maximum-number-of-eaten-apples/main.cpp
@@ -0,0 +1,98 @@
+// https://leetcode.com/problems/maximum-number-of-eaten-apples/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  int eatenApples(vector<int>& apples, vector<int>& days) {
+    int N = apples.size();
+
+    map<ll, ll> day_to_ct;
+
+    ll res = 0;
+
+    rep(i, 1e5) {
+      if (i < N) {
+        ll a = apples[i];
+        ll d = days[i];
+
+        day_to_ct[i + d - 1] += a;
+      }
+
+      for (auto& [k, v] : day_to_ct) {
+        if (k < i || v == 0) {
+          day_to_ct.erase(k);
+          continue;
+        }
+
+        v--;
+        res++;
+        break;
+      }
+    }
+
+    return res;
+  }
+};
---
+++ problems/where-will-the-ball-fall/main.cpp
@@ -0,0 +1,103 @@
+// https://leetcode.com/problems/where-will-the-ball-fall/
+
+#include "algorithm"
+#include "bitset"
+#include "cmath"
+#include "functional"
+#include "iomanip"
+#include "iostream"
+#include "map"
+#include "numeric"
+#include "queue"
+#include "set"
+#include "string"
+#include "unordered_map"
+#include "unordered_set"
+#include "vector"
+#define rep(i, to) for (ll i = 0; i < (to); ++i)
+#define rep1(i, to) for (ll i = 1; i <= (to); ++i)
+#define repf(i, from, to) for (ll i = from; i < (to); ++i)
+#define repr(i, from) for (ll i = from - 1; i >= 0; --i)
+#define all(vec) vec.begin(), vec.end()
+#define unless(cond) if (!(cond))
+#define fi first
+#define se second
+using namespace std;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using V = vector<T>;
+using VL = V<ll>;
+template <typename T>
+using VV = V<V<T>>;
+using VVL = VV<ll>;
+template <typename T>
+using VVV = VV<V<T>>;
+template <typename T, typename U>
+using P = pair<T, U>;
+using PL = P<ll, ll>;
+using VPL = V<PL>;
+template <typename T>
+using asc_pq = priority_queue<T, V<T>, greater<T>>;
+template <typename T>
+inline bool chmax(T& a, T b);
+template <typename T>
+inline bool chmin(T& a, T b);
+
+const ll INF = 1e18;
+
+template <typename T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+class Solution {
+ public:
+  vector<int> findBall(vector<vector<int>>& grid) {
+    ll H = grid.size();
+    ll W = grid[0].size();
+
+    vector<int> res(W);
+
+    rep(w, W) {
+      ll cur = w;
+
+      rep(h, H) {
+        ll cd = grid[h][cur];
+
+        if (cd == 1) {
+          unless(cur + 1 < W && grid[h][cur + 1] == 1) {
+            cur = -1;
+            break;
+          }
+
+          cur++;
+        } else if (cd == -1) {
+          unless(cur - 1 >= 0 && grid[h][cur - 1] == -1) {
+            cur = -1;
+            break;
+          }
+
+          cur--;
+        }
+      }
+
+      res[w] = cur;
+    }
+
+    return res;
+  }
+};
